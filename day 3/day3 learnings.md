428bc46e8139ab9aaaeda36f08e535e889c21cdb
alex-fili-git <alex_filippo@icloud.com>
2022-10-13 10:08:06 +0200

    finished day 3

---
+++ day 3/day3 learnings.md
@@ -0,0 +1,12 @@
+# conditional statements, logical operators, code blocks and scopes
+double strings conditions does not work with '&' but works with 'and'
+
+string.count("letter of choice") counts how often a letter occurs in string
+
+three single quotes to select huge print:
+print('''
+
+
+''')
+
+Use ' in print \'
---
+++ day 3/treasure_island.py
@@ -0,0 +1,49 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+a = input("You arrived at a crossroad which way do you want to go? Left or right?\n")
+if a.lower() != "left":
+    print("You fell into a hole. Game over.")
+    exit()
+else:
+    b = input("Swim or wait?\n")
+    if b.lower() != "wait":
+        print("Ouch, attacked by trout. Game over.")
+        exit()
+    else:
+        c = input("Red, Yellow or Blue door?\n")
+        if c.lower() == "yellow":
+            print("You win!")
+            exit()
+        elif c.lower() =="blue":
+            print("Eaten by beasts. Game Over.")
+        elif c.lower() =="red":
+            print("Burned by fire. Game Over.")
+        else:
+            print("Game over.")
