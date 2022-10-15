6878aac4b203961b22c52021c35e7326bc031bda
alex-fili-git <alex_filippo@icloud.com>
2022-10-15 08:36:12 +0200

    finished day 7

---
+++ day 7/day7 learnings.md
@@ -0,0 +1,2 @@
+# hangman project
+making a hangman game.
--- day 7/hangman.py
+++ day 7/hangman.py
@@ -1,9 +1,41 @@
-#Step 1 
+import random
+from re import I
+import hangman_art
+import hangman_words
 
-word_list = ["aardvark", "baboon", "camel"]
+end_game = False
+lives = 6
+#pick a random word from the list
+chosen_word = random.choice(hangman_words.word_list)
+print(chosen_word)
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+print(hangman_art.logo)
+#create blank display
+display = ['_']*len(chosen_word)
+print(hangman_art.stages[lives])
+print(' '.join(map(str, display)))
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+while not end_game:
+    #ask for guess
+    guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"you already guessed {guess}. Pick a new one.")
+    else:
+        if guess in chosen_word:
+            for index, letter in enumerate(chosen_word):
+                if guess == letter:
+                    display[index] = letter
+        else:
+            print("Letter is not in chosen word. You lost a life.")
+            lives -= 1
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+        if "_" not in display:
+            end_game = True
+            print("you win")
+        elif lives == 0:
+            end_game = True
+            print("You lose")
+        #print current state
+        print(hangman_art.stages[lives])
+        print(' '.join(map(str, display)))
+
