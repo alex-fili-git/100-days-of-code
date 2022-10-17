5c92dac8f1d59521c37a2253d7875a0abbb91b85
alex-fili-git <alex_filippo@icloud.com>
2022-10-17 10:03:02 +0200

    started day 10

---
+++ day 9/FPSB_auction.py
@@ -0,0 +1,15 @@
+#First-price sealed-bid auction
+import art
+import operator
+import os
+print(art.logo)
+biddings = {}
+more_biddings = "yes"
+while more_biddings == "yes":
+    name = input("What is your name?: ")
+    bid = input("What is your bid?: ")
+    biddings[name] = bid
+    more_biddings = input("Are there any more bidders? Type 'yes' to add a bidding and 'no' otherwise: ").lower()
+    if more_biddings == "yes":
+        os.system('clear')
+print(f"The winner is {max(biddings.items(), key = lambda k : k[1])[0]} with {max(biddings.items(), key = lambda k : k[1])[1]}.")
