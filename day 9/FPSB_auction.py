e55258af69f93825e6d0f38b2ffc9976645dfd79
alex-fili-git <alex_filippo@icloud.com>
2022-10-17 10:10:11 +0200

    finished day 9

--- day 9/FPSB_auction.py
+++ day 9/FPSB_auction.py
@@ -7,9 +7,9 @@ biddings = {}
 more_biddings = "yes"
 while more_biddings == "yes":
     name = input("What is your name?: ")
-    bid = input("What is your bid?: ")
+    bid = int(input("What is your bid?: "))
     biddings[name] = bid
     more_biddings = input("Are there any more bidders? Type 'yes' to add a bidding and 'no' otherwise: ").lower()
     if more_biddings == "yes":
         os.system('clear')
-print(f"The winner is {max(biddings.items(), key = lambda k : k[1])[0]} with {max(biddings.items(), key = lambda k : k[1])[1]}.")
+print(f"The winner is {max(biddings.items(), key = lambda k : k[1])[0]} with ${max(biddings.items(), key = lambda k : k[1])[1]}.")
