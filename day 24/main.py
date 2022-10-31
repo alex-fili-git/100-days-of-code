71ef040c31edd72557ef5c5bae92c3eb685d36eb
alex-fili-git <alex_filippo@icloud.com>
2022-10-31 11:58:31 +0100

    finished day 24

--- day 24/main.py
+++ day 24/main.py
@@ -2,13 +2,9 @@
 with open("./Input/Names/invited_names.txt") as f:
     names = f.readlines()
 
+
 for name in names:
-
-#for each name in invited_names.txt
-
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+    name = name.strip("\n")
+    end = open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w")
+    with open("./Input/Letters/starting_letter.txt") as start:
+        end.write(start.read().replace("[name]", name))
